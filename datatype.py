030dd99169b62e9814c8a5fe92b0f54885b4d703
davadadinal-20 <dinaldavada@gmail.com>
2026-01-06 08:14:01 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,27 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("helloworld")
+helloworld
+x=20
+print(x)
+20
+y=60.5
+print(y)
+60.5
+d=30j
+print(d)
+30j
+s=["dinal","dhruvi","krishna"]
+print(s)
+['dinal', 'dhruvi', 'krishna']
+t=("apple","banana","cherry")
+print(t)
+('apple', 'banana', 'cherry')
+o=true
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    o=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+o=True
+print(o)
+True
