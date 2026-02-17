e44f34da6d4f434a5fb9c01d0f1cacc037870e9a
davadadinal-20 <dinaldavada@gmail.com>
2026-02-17 07:48:16 +0530

    Add files via upload

---
+++ forloop1.py
@@ -0,0 +1,12 @@
+#print numbers from 1to 10#
+"""for i in range (1,11):
+    print(i)"""
+#print even numbers from 1to15
+"""for i in range (1,21):
+    if i %2==0:
+        print(i)""""
+#print odd numbers from 1 to 15
+for i in range(1,16):
+    if i % 2!=0:
+        print(i)
+
