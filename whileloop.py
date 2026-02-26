d2d8f7268e87810e1ffc9334a751939be9ee13a9
davadadinal-20 <dinaldavada@gmail.com>
2026-02-26 10:00:29 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+# print 1 to 5 number
+i=1
+while i<=5:
+    print(i)
+    i=i+1
+# print numbers from 1 to 10
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+    # sum of first n natural numbers
+    n=int(input("enter number:"))
+    i=1
+    s=0
+    while i<=n:
+        s=s+i
+        i=i+1
+        print("sum=",s)
